91a77d7c2cf06bda2c33f3bb3989338628a50517
emmanuelkoech1820 <emmanuelkoech1820@gmail.com>
2020-05-03 10:05:07 +0300

    Commit

---
+++ Element not repeated/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class NotRepeated
+{
+    //Main program
+    // This program has O(n) time complexity, O(1) space complexity
+    public static void Main()
+    {
+        int[] arr = { 4, 5, 6, 7, 5, 4, 6 };
+        int n = arr.Length;
+        Console.WriteLine("Element Not Repeated Is " +
+            findNotRepeated(arr, n) + "");
+    }
+
+
+    static int findNotRepeated(int[] arr, int arr_size)
+    {
+        int res = arr[0];
+
+        // Do XOR(^) comparison
+        for (int i = 1; i < arr_size; i++)
+            res = res ^ arr[i];
+        return res;
+    }
+}
+
